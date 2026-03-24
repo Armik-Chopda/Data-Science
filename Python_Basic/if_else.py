d601842e401e50dea8cb11fe57dcbb179bfb6e2f
Armik Chopda <armikchopda35280@gmail.com>
2026-03-24 09:17:29 +0530

    if_else

---
+++ Python_Basic/if_else.py
@@ -0,0 +1,10 @@
+marks=int(input("Enter your marks"))
+if marks >=90:
+    print("A+")
+elif marks <90 and marks>=80:
+    print("A1")
+elif marks<80 and marks>=60:
+    print("A2")
+
+else:
+    print("A3")

fb037ee76709c8e55e7526eb4dd78f45d07d7cb8
Armik Chopda <armikchopda35280@gmail.com>
2026-03-24 10:07:05 +0530

    comprehension

---
+++ Python_Basic/Comprehension_Oprations.py
@@ -0,0 +1,21 @@
+l=[1,2,4,5,6,7,8,9]
+# list comprehension
+s=[i**2 for i in l]
+print(s)
+l1=["armik","vasu","meet","krishna","hit","prince"]
+l2=[i.upper() for i in l1]
+print(l2)
+x=[i for i in l if i%2==0]
+print(x)    
+
+# tuple comprehension
+t=("armik","see",23,44,55,)
+t1=[i for i in t if type(i)==str]
+t2=[i for i in t if type(i)==int]
+print(t1)
+print(t2)
+
+#disk comprehension
+armik={"armik":8,'dhruv':90,'jeel':78,'vasu':56}
+ar={i:j*2 for i,j in armik.items()}
+print(ar)
